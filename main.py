c2bf6224c34ed2eae74e20cd1c784a68408a55ad
0002itz <ortegagit@gmail.com>
2024-10-27 11:24:25 -0500

    perdida de datos, por ley de jerarquia

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
