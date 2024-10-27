87a0a3b577166d1c9d817340434b41cf231752f0
0002itz <ortegagit@gmail.com>
2024-10-26 23:43:20 -0500

    segundo comit itme-id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message":"Hello Word!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
