e3b3f846353d06f63246cba77e31207c7a42ea3b
0002itz <ortegagit@gmail.com>
2024-11-07 21:53:16 -0500

    creacion de DATABASE con CRUD.

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
-from fastapi import FastAPI
+import sqlite3 as sql
 
-app = FastAPI()
+def createDB():
+    conn =sql.connect("test-kuku.db")
+    conn.commit()
+    conn.close()
 
+def createTabla():
+    conn =sql.connect("./db/test-kuku.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE Users (
+        userID INTEGER PRIMARY KEY AUTOINCREMENT,
+        userNickname TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        ps TEXT NOT NULL
+        )"""
+    )
+    conn.commit()
+    conn.close()
+    print("Table Create.")
 
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
+def insertData(userNickname,Email,ps):
+    conn =sql.connect("./db/test-kuku.db")
+    cursor = conn.cursor()
+    data = "INSERT INTO Users (userNickname,Email,ps) Values (?,?,?)"
+    cursor.execute(data,(userNickname,Email,ps))
+    conn.commit()
+    conn.close()
+    print("Insertion Complit.")
 
+def selctData():
+    conn=sql.connect("./db/test-kuku.db")
+    cusrsor=conn.cursor()
+    cusrsor.execute(
+        """
+        SELECT userID
+        FROM Users;
+        """
+    )
+    conn.commit()
+    conn.close()
+    print("SELECT USER ID.")
 
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
+def delateUserById():
+    conn = sql.connect("./db/test-kuku.db")
+    cursor = conn.cursor()
+    cursor.execute("DELeTE FROM Users WHERE userID=2;")
+    conn.commit()
+    conn.close()
+    print("User delate in Table Users.")
+
+def delateTable():
+    conn = sql.connect("./db/test-kuku.db")
+    cursor = conn.cursor()
+    cursor.execute(
+    """
+    DROP TABLE Users;
+    """
+    )
+    conn.commit()
+    conn.close()
+    print("Table eliminate.")
+
+if __name__ == "__main__":
+    delateUserById()
