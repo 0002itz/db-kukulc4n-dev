a6626d9683a5a23ba0324e57f2de1dec36181ad9
0002itz <ortegagit@gmail.com>
2024-11-10 19:31:48 -0500

    CRUD,cambio de nombre de funciones.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 from fastapi import FastAPI, HTTPException
-
 from cryptography.fernet import Fernet
+
 key = Fernet.generate_key()
 f = Fernet(key)
 
@@ -12,52 +12,55 @@ def getConectDB():
     conn.row_factory = sql.Row
     return conn
 
-@app.get("/users/")
-async def getUsers():
-    conn = getConectDB()
-    c = conn.cursor()
-    c.execute( "SELECT * FROM Users" )
-    users = c.fetchall()
-    conn.close()
-    return { "AllUsers": users }
-
-@app.get("/userByID/{userID}")
-async def getUserID(userID:int):
-    conn = getConectDB()
-    c = conn.cursor()
-    c.execute( f'SELECT userID FROM Users WHERE userid={userID}' )
-    userid = c.fetchone()
-    c.close()
-    if userid is None:
-        raise HTTPException(status_code=404, detail="UserID not found.")
-    return { "UserID": userid[0] }
-
-@app.post("/createrUser/")
+@app.post("/newUser/")
 async def createUser(Nickname,Email,ps):
     conn = getConectDB()
     c = conn.cursor()
     ps = f.encrypt(b'ps')
     data_insert = "INSERT INTO Users (userNickname,Email,ps) Values (?,?,?);"
-    c.execute(data_insert,(Nickname,Email,ps))
-    newUser = c.fetchone()
+    c.execute(data_insert,(Nickname,Email,ps,))
     conn.commit()
     c.close()
-    if newUser is None:
-        raise HTTPException(status_code=202,detail="New User Create.")
-    return{ "User Create": newUser }
+    return{ "User Create": Nickname }
 
-@app.post("/upDatePW/")
-async def apdate():
-    pass
+@app.get("/Users/{Users}")
+async def allUsers():
+    conn = getConectDB()
+    c = conn.cursor()
+    c.execute( "SELECT * FROM Users" )
+    users = c.fetchall()
+    c.close()
+    return { "AllUsers": users }
 
-@app.delete("/delateUserByID/{userID}")
-async def delateUserById(userID:int):
+@app.get("/User/{userID}")
+async def user(userID:int):
+    conn = getConectDB()
+    c = conn.cursor()
+    userdata = "SELECT * FROM Users WHERE userID=?;"
+    c.execute(userdata,(userID,))
+    userdata = c.fetchone()
+    c.close()
+    if userdata is None:
+        raise HTTPException(status_code=404, detail="User not found.")
+    return { "User SELECT": userdata }
+
+@app.put("/newNickname/{Nickname}")
+async def ChangeNickname(newNickname:str,oldNickname:str):
+    conn = getConectDB()
+    c = conn.cursor()
+    update="UPDATE Users SET userNickname = ? WHERE userNickname = ?;"
+    c.execute(update,(newNickname,oldNickname,))
+    conn.commit()
+    c.close()
+    return { "Nickname Update complet" : newNickname }
+
+@app.delete("/delateUser/{userID}")
+async def delateUser(userID:int):
     conn = getConectDB()
     c = conn.cursor()
     data_delate = f'DELETE FROM Users WHERE userID={userID};'
     c.execute(data_delate)
-    usertoDelate = c.fetchone()
     conn.commit()
     c.close()
     print("User Delate.")
-    return { "User Delate": usertoDelate[0] }
+    return { "User Delate": f'ID {userID}' }
